5e3e5a21de6d31f185258ee5b363e94ae4b59322
wisetree2939 <serafymsafonov01@gmail.com>
2025-09-13 11:06:31 -0700

    Change Bounce script

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -6,30 +5,46 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private int _playerSpeed;
+    [SerializeField] private float _playerSpeed = 5f; // movement speed
+    [SerializeField] private float _bounceForce = 10f; // how strong the bounce is
     [SerializeField] private Rigidbody _rigidbody;
-    private float _bounceStreak;
+    
 
+    private int _bounceStreak;
     public TextMeshProUGUI BounceStreakText;
 
     private void Update()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
-        
-        Vector3 moveDirection = new Vector3(moveHorizontal,0, 0);
+
+        Vector3 moveDirection = new Vector3(moveHorizontal, 0, 0);
         transform.Translate(moveDirection * _playerSpeed * Time.deltaTime);
     }
 
     private void OnCollisionEnter(Collision other)
     {
-        if (other.gameObject.tag == "bouncy")
+        if (other.gameObject.CompareTag("bouncy"))
         {
-            _bounceStreak += 1;
+            // Reset Y velocity before applying bounce (so bounces are consistent)
+            Vector3 velocity = _rigidbody.velocity;
+            velocity.y = 0;
+            _rigidbody.velocity = velocity;
+
+            // Apply upward bounce force
+            _rigidbody.AddForce(Vector3.up * _bounceForce, ForceMode.Impulse);
+
+            // Update streak
+            _bounceStreak++;
+            if (_bounceForce < 7)
+            {
+                _bounceForce += 0.05f;
+            }
             BounceStreakText.text = _bounceStreak.ToString();
         }
         else
         {
             _bounceStreak = 0;
+            BounceStreakText.text = _bounceStreak.ToString();
         }
     }
 }
